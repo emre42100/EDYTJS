6e8d18756ea7a201f7b067b852add8678b5b76de
emre42100 <58374221+emre42100@users.noreply.github.com>
2021-10-18 23:03:43 +0200

    deneme

---
+++ src/app.js
@@ -0,0 +1,26 @@
+console.log("Merhaba kodlama.io");
+
+let dolarDun   = 9.20
+let dolarBugun = 9.30
+
+{
+    let dolarDun = 9.10
+}
+
+console.log(dolarBugun)
+
+const eurDun = 11.2
+      //eurDun = 11
+      console.log(eurDun)
+
+      let konutKredileri = ["konut kredisi","Emlak konut kredisi","kamu konut kredisi","ozel konut kredisi"];
+      console.log(konutKredileri)
+
+      console.log("<ul>")
+
+      for (let i = 0; i < konutKredileri.length; i++) {
+          
+      console.log("<li>"+konutKredileri[i]+"</li>")
+          
+      }
+      console.log("</ul>")
